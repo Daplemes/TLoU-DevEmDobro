e7c207a844f55afe6b827c067efb7dea3fb8d66d
Daplemes <120739820+Daplemes@users.noreply.github.com>
2023-03-30 19:15:46 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botoesCarrossel = document.querySelectorAll('.botao')
+const imagensCarrossel = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.selecionado')
+        botaoSelecionado.classList.remove('selecionado')
+        botao.classList.add('selecionado')
+
+        const imagemAtiva = document.querySelector('.ativa')
+        imagemAtiva.classList.remove('ativa')
+        imagensCarrossel[indice].classList.add('ativa')
+    })
+})
